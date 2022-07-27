5421f6313def18e52004e85edddc15cd8369350b
Batman0k <af2006123@gmail.com>
2022-07-27 12:32:42 +0000

    last task done

--- 5.c
+++ 5.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int units_consumed = 0, total_bill = 0;
+    float units_consumed = 0, total_bill = 0;
 
     printf("Enter units consumed: ");
-    scanf("%d", &units_consumed);
+    scanf("%f", &units_consumed);
 
     if(units_consumed <= 50)
     {
@@ -18,7 +18,31 @@ int main() {
     {
         total_bill = units_consumed * 1.2;
     }
+    else if (units_consumed > 200)
+    {
+        total_bill = units_consumed * 1.5;
+    }
 
-
-    prinft("Your electricity bill is, %d", total_bill)
+    printf("Your electricity bill is, %6.2f", total_bill);
 }
+
+/*
+    float units_consumed = 0, total_bill = 0
+
+    print("Enter units consumed: ")
+    input(units_consumed)
+
+    if(units_consumed <= 50)
+        total_bill = units_consumed * 0.5
+
+    else if (units_consumed <= 100)
+        total_bill = units_consumed * 0.75
+
+    else if (units_consumed <= 200)
+        total_bill = units_consumed * 1.2
+
+    else if (units_consumed > 200)
+        total_bill = units_consumed * 1.5
+
+    print("Your electricity bill is, ", total_bill)
+*/
